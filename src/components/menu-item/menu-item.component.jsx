299fce29d63b06f647e46fdf3a6486933222e56b
Umair Hafeez <umair.hafeez@rci.rogers.com>
2020-01-19 23:49:18 -0500

    homepage broken into directory and menu-items with styling

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './menu-item.styles.scss';
+
+const MenuItem = ({title, imageUrl, size}) => (
+  <div
+    className={`menu-item ${size}`}>
+    <div className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    ></div>
+    <div className='content'>
+      <div className='title'>{title.toUpperCase()}</div>
+      <span className='subtitle'>SHOP NOW</span>
+    </div>
+  </div>
+)
+
+export default MenuItem;
